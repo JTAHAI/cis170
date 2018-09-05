92a123f3787ecfe8a2f681695a6556197f836e78
Justin Tahai <sJustinTahai@yccc.edu>
2018-09-05 19:02:11 -0400

    First Commit

---
+++ src/project1/HelloWorld.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class HelloWorld 
+{
+	// An object is 2 things.
+	// 1. Actions. Methods.
+	// 2. Attributes. Variables.
+	private String name = "Justin";
+	
+	// main method. Starts the program.
+	//Method signature - What makes this method unique. Defines the method.
+	public static void main(String[] args) 
+	{
+		// Prints to the console.
+		System.out.println("Hello World");
+		System.out.println("My first java app!!!");
+	}
+}
