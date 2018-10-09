48b87ec7fcf17d1cfd2f113296b5265cb8a50cdf
Justin <sjustintahai@yccc.edu>
2018-10-09 12:13:13 -0400

    homework

--- src/edu/tahai/java/week5/Hangman.java
+++ src/edu/tahai/java/week5/Hangman.java
@@ -6,14 +6,18 @@ public class Hangman
 	
 		public static void main(String[] args)
 	{
+			System.out.println("WELCOME TO HANGMAN!");
+			System.out.println("Here is your word:");
 		//call createMask to print mask to console
 			Loops l = new Loops();
 			l.createMask();
-			
+		//call inputLoop	
+			Loops i = new Loops();
+			i.inputLoop();
 		//call gameLoop to play
 			Loops g = new Loops();
 			g.gameLoop();
-			
+
 
 	}
 }
--- src/edu/tahai/java/week5/Loops.java
+++ src/edu/tahai/java/week5/Loops.java
@@ -11,9 +11,6 @@ public class Loops
 	//create word array
 	String[] wordsArr = {"HORSE", "CAR", "HOME", "SCHOOL", "JAVA", "COMPUTER", "SCIENCE"};
 	
-	//create alphabet array
-	String[] alphabetArr = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-	
 	//create guessed letter array
 	String[] guessedLetters = {};
 	
@@ -24,64 +21,72 @@ public class Loops
 	//choose and then store a word from word array as variable for current game using random number
 	String gameWord = wordsArr[n];
 
-
+	Scanner scanner = new Scanner(System.in);
 
 	//create mask
 	
 	public void createMask()
 	{
 		
-
 	//create initial mask
+
 		for(int i = 0; i < gameWord.length(); i++)
 		{
 			System.out.print("*");
 		}
 	}
 	
-	
+	public void inputLoop()
+	{
+		//prompt user to guess a letter
+		System.out.println();
+		System.out.println("Guess a letter: ");
+
+	    while (!scanner.hasNext("[A-Za-z]+")) 
+	    {
+	        System.out.println("Letters only! Guess a letter:");
+	        scanner.nextLine();
+	    }
+
+	}
 	//create game play loop
-	@SuppressWarnings("resource")
 	public void gameLoop()
 	{
 
 	//insert loop logic
-		
-		
-	//prompt user to guess a letter
-		System.out.println();
-		System.out.println("Guess a letter: ");
-		Scanner scanner = new Scanner(System.in);
-		String guessedLetters = scanner.nextLine();
-		
+
 		
 	//check if user input is contained in gameWord
-	    if(gameWord.contains(guessedLetters.toUpperCase()))
+	    if(gameWord.contains(scanner.next()))
+	    {
 	        System.out.println("You got a letter!");
-	    else
+	    }
+	        else
+	    {
 	    	System.out.println("Fail...");
 	    	g--;
-	    	
+	    }
 
 	
 	    
 	//create and display new mask
-	    if(gameWord.contains(guessedLetters.toUpperCase()))
-	    	
-	    	// use letter position in logic to create new partial mask
-	        System.out.print("*");
+
 		
 		
 		
 	//display list of letters guessed
-		System.out.println("You've guessed: " + guessedLetters.toUpperCase());
+		System.out.println("You guessed: " + scanner.next());
 		
 	//display state of "hang man" (number of incorrect guesses)
 		if(g > 0)
+		{
 		System.out.println("You have " + g + " incorrect guesses remaining.");
+		}
 		else
-		System.out.println("GAME OVER.");	
+		{
+		System.out.println("GAME OVER.");
 
+		}
 		    	
 		
 
