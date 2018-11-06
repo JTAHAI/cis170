7c172fb340a9e4bd683074fd0389d8f82f360429
Justin <sjustintahai@yccc.edu>
2018-11-06 12:41:35 -0500

    homework

--- src/edu/tahai/java/hangmanproject2/App.java
+++ src/edu/tahai/java/hangmanproject2/App.java
@@ -1,5 +1,93 @@
 package edu.tahai.java.hangmanproject2;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+
+public class App 
+{
+
+
+	// load game from file *****************************************************
+	private List<Game> loadGames() throws IOException
+	{
+		List<Game> game = new ArrayList<Game>();
+		File f = new File("games.txt");
+	
+		if(f.exists())
+		{
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			
+			String st;
+			while ((st = br.readLine()) != null)
+			{
+				String[] splitGames = st.split(" ");
+				Game g = new Game();
+				u.setHint(splitGames[0]);
+				u.setWord(splitGames[1]);
+				}
+				game.add(g);
+			}
+		}
+			return users;
+	}
+	
+	
+	
+	
+	
+	// number of allowed guesses
+	private int guessCount = 6;
+	
+	// Control flow of game.
+	public void playGame()
+	{
+
+		// change to file buffered reader ******************** no longer an array ****************************************
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < gArray.length; i++)
+		{
+		Game g = gArray[i];
+		// number of allowed guesses
+		int guessCount = 6;
+		
+		
+		// Repeat while not solved
+		while(!g.isSolved() && guessCount > 0)
+		{
+			// Prompt the user
+			System.out.println("Guess a letter ");
+			System.out.println("Hint " + g.getHint());
+			// Display current mask
+			System.out.println("Mask " + g.generateMask());
+			// Collect user input
+			String userGuess = sc.nextLine();
+			g.setGuesses(userGuess.toLowerCase());
+			g.setGuesses(userGuess.toUpperCase());
+			
+			guessCount --;
+			System.out.println("You have " + guessCount + " guesses remaining.");			
+		}
+		
+		// play again?
+		if(g.isSolved())
+		{
+			System.out.println("Play again? y/n?");
+			String playAgain = sc.nextLine();
+
+		}
+		}
+	}
+	public static void main(String[] args)
+	{
+		App app = new App();
+		app.playGame();
+
+	}
 }
--- src/edu/tahai/java/hangmanproject2/Game.java
+++ src/edu/tahai/java/hangmanproject2/Game.java
@@ -1,5 +1,76 @@
 package edu.tahai.java.hangmanproject2;
 
-public class Game {
+public class Game 
+{
 
+	private String hint;
+	private String word;
+	private String guesses = "";
+
+	// Arg Constructor
+	public Game()
+	{
+		this.hint = hint;
+		this.word = word;
+	}
+
+	
+	public String getHint() 
+	{
+		return hint;
+	}
+	public void setHint(String hint) 
+	{
+		this.hint = hint;
+	}
+	public String getWord() 
+	{
+		return word;
+	}
+	public void setWord(String word) 
+	{
+		this.word = word;
+	}
+	public String getGuesses() 
+	{
+		return guesses;
+	}
+	public void setGuesses(String guesses) 
+	{
+		this.guesses += guesses;
+	}
+	
+	public boolean isSolved()
+	{
+		// Do smart things to determine if solved
+		return generateMask().equals(word);
+	}
+	
+	public String generateMask()
+	{
+		String mask = "";
+		
+		// Loop over word
+		for(int i = 0; i < word.length(); i ++)
+		{
+			// Look at each character in word
+		String currentChar = String.valueOf(word.charAt(i));
+
+				
+			// Determine if user guessed that char.
+			if(guesses.contains(currentChar))
+			{
+			//  If they did, append the char.
+				mask += currentChar;
+			}
+			else 
+			{
+			// If they did not, append a *.
+				mask += "*";
+			}
+			
+
+		}
+		return mask;
+	}
 }
